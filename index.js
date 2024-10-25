ca06746d6d98fcf5756b621562f92a53827e63c1
Nele Ulvi <nele.ulvi@voco.ee>
2024-10-25 15:05:48 +0300

    Update single task

--- index.js
+++ index.js
@@ -36,15 +36,27 @@ app.get('/', (req, res)=>{
     //get data from file
     readFile('./tasks.json')
         .then((tasks) => {
-            res.render('index', {tasks: tasks})
+            res.render('index', {
+                tasks: tasks,
+                error:null
+        })
         })
 })
 //for parsing application /x-www-form-urlencoded
 app.use(express.urlencoded({ extended:true}))
 
 app.post('/', (req, res) =>{
-    console.log('form sent data')
-    let task = req.body.task
+    let error = null
+    if (req.body.task.trim().length == 0){
+        error = "please insert correct task data"
+        readFile('./tasks.json')
+        .then(tasks =>{
+            res.render('index', {
+                tasks:tasks,
+                error:error
+            })
+        })
+    }else 
     //get data from file
     readFile('./tasks.json')
         .then((tasks) => {
@@ -64,7 +76,8 @@ app.post('/', (req, res) =>{
             data = JSON.stringify(tasks, null, 2)
             writeFile('./tasks.json', data)
             res.redirect('/')
-        })
+        }) 
+
 })
 
 app.get('/delete-task/:taskId', (req, res) => {
@@ -89,6 +102,63 @@ app.get('/delete-tasks', (req, res) =>{
     res.redirect('/')
 
 })
+
+app.post('/update-task', (req, res) =>{
+    let updateTaskId = parseInt(req.params.taskId)
+    let updateTask = req.body.task
+    let error = null
+    if (updateTask.trim().length === 0){
+        error = "please insert correct task data"
+        readFile('./tasks.json')
+        .then(tasks =>{
+            res.render('index', {
+                tasks:tasks,
+                error:error 
+            })
+        })
+    }else {
+        readFile('./tasks.json')
+            .then((tasks) => {
+                let index
+                if(tasks.length === 0)
+                {
+                    index = 0
+                }else{
+                    index = tasks[tasks.length-1].id + 1;
+                }
+                const newTask = {
+                    "id": index,
+                    "task": req.body.task
+                }
+                tasks.push(newTask)
+    
+                data = JSON.stringify(tasks, null, 2)
+                writeFile('./tasks.json', data)
+                res.redirect('/')
+            }) 
+        }
+})
+
+app.get('/update-task/:taskId', (req, res) =>{
+    let updateTaskId = parseInt(req.params.taskId)
+    readFile('./tasks.json')
+    .then(tasks => {
+        let updateTask
+        tasks.forEach((task) =>{
+            if(task.id === updateTaskId){
+                updateTask = task.task
+            }
+        })
+        console.log(updateTask)
+        res.render('update', {
+        updateTask: updateTask,
+        updateTaskId: updateTaskId,
+        error: null
+        })
+    
+    })
+})
+
 
 app.listen(3001, () =>{
     console.log ('Example app is started at http://localhost:3001')
